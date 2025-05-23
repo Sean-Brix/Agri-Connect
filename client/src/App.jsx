c5e465f6216609fd53ab154477ecb5b125e05d96
Rhenzy H. Cruzat <rhenzycruzat254@gmail.com>
2025-05-23 22:26:03 +0800

    v0.3.0 - Survey Modals

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,13 +5,14 @@ import Reg_2 from './Services/Authentication/Components/Reg_2.jsx'
 import Reg_3 from './Services/Authentication/Components/Reg_3.jsx'
 import Landing from './Services/Landing_Page/Landing/Landing.jsx'
 import EIC from './Services/EIC_Borrow/Client/EIC.jsx'
+import Survey from './Components/Survey/Survey.jsx'
 
 function App() {
     
   return (
     < >
 
-      <EIC />
+      <Survey />
       
      
     
--- client/src/Services/EIC_Borrow/Client/EIC.jsx
+++ client/src/Services/EIC_Borrow/Client/EIC.jsx
@@ -34,18 +34,24 @@ export default function EIC() {
     return (
         <>
             <Navbar>
-                <h2 className="text-2xl font-bold mb-4 mt-20">Available Equipment</h2>
-                <div className="flex flex-col items-center bg-gradient-to-b from-blue-600 to-blue-800 text-white mt-10 rounded-lg max-h-[70vh] overflow-y-auto p-4">
+                 <div className="relative mt-30">
+                            <hr className="border-black-300" />
+                            <span className="absolute left-1/8 -translate-x-1/4 -top-5 bg-white  rounded-lg px-4 text-2xl font-semibold text-gray-700">
+                                Available Equipments
+                            </span>
+                        </div>
+
+                <div className="flex flex-col items-center bg-gradient-to-b from-blue-600 to-blue-800 text-white mt-10 w-full rounded-lg max-h-[70vh] overflow-y-auto p-10">
                     
                     {equipmentList.map((item, idx) => (
-                        <div key={idx} className="bg-white p-4 rounded-lg shadow-md flex flex-row items-center w-full max-w-6xl mx-auto mb-8">
-                            <img src={item.img} alt={item.name} className="w-1/2 bg-blue-600 h-48 object-cover rounded-md mb-4" />
+                        <div key={idx} className="bg-white p-4 rounded-lg shadow-md flex flex-row flex-wrap items-center w-full max-w-6xl mx-auto   mb-8 ">
+                            <img src={item.img} alt={item.name} className="w-full bg-blue-800 h-48 object-cover rounded-md mb-4  "  />
                             <div className="flex flex-col justify-center p-5">
                                 <h3 className="text-xl font-semibold mb-2 text-black">{item.name}</h3>
                                 <p className="text-gray-600 mb-4">{item.desc}</p>
                                 <div className="flex space-x-2">
-                                    <button className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600">Borrow</button>
-                                    <button className="bg-gray-300 text-gray-700 px-4 py-2 rounded-md hover:bg-gray-400">Schedule</button>
+                                    <button className="bg-blue-500 text-white px-4 rounded-md border-2 border-black bg-blue-800 hover:bg-blue-600">Borrow</button>
+                                    <button className="bg-gray-300 text-black px-4 py-2 rounded-md bg-white border-2 border-black hover:bg-gray-200">Details</button>
                                 </div>
                             </div>
                         </div>
