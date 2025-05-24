0754e21325d02175cc6a0c67112a281ec1972075
Kc Sean Brix Calinao <kcseancalinao@gmail.com>
2025-05-25 00:58:18 +0800

    v0.5.2 - Error handling test for response. A slight addition to API_Response.php TEST,

--- server/API/TEST/API_Response.php
+++ server/API/TEST/API_Response.php
@@ -10,5 +10,10 @@ $req = getJsonBody();
 $data1 = $req['data1'];
 $data2 = $req['data2'];
 
+// Error Handling
+if($data1 == "" || $data2 == ""){
+    sendResponse(400, "Failed", [], "Bad Request: All Data is Required");
+}
+
 // Response
 sendResponse(200, "Success", [$data1, $data2], "Successfully recieved the data");
