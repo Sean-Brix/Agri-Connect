413f0f1027e5f0b729f1d8a6eafcc0352e923bd2
Rhenzy H. Cruzat <rhenzycruzat254@gmail.com>
2025-05-26 22:04:42 +0800

    v0.9.1 - fixing bg color or eic and enrollment

--- client/src/Services/EIC_Borrow/Client/EIC.jsx
+++ client/src/Services/EIC_Borrow/Client/EIC.jsx
@@ -59,7 +59,7 @@ export default function EIC() {
                     </span>
                 </div>
 
-                <div className="bg-gradient-to-b from-gray-400 to-gray-200 text-white mt-10 w-full rounded-lg max-h-[70vh] overflow-y-auto p-10">
+                <div className="bg-gradient-to-b from-blue-400 to-blue-200 text-white mt-10 w-full rounded-lg max-h-[70vh] overflow-y-auto p-10">
                     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
                         {equipmentList.map((item, idx) => (
                             <div key={idx} className="bg-white p-4 rounded-lg shadow-md flex flex-col items-center w-full mb-8 relative" style={{ minHeight: '350px' }}>
