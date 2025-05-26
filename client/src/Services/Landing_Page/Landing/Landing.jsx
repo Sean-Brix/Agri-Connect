1d5f198b2881070c491709bb0de849edd9fc0afb
Rhenzy H. Cruzat <rhenzycruzat254@gmail.com>
2025-05-26 21:35:42 +0800

    v0.8.8 - making the landing page texts responsive

--- client/src/Services/Landing_Page/Landing/Landing.jsx
+++ client/src/Services/Landing_Page/Landing/Landing.jsx
@@ -21,14 +21,14 @@ export default function Landing() {
                                 <h1 className="font-semibold text-2xl mr-4 0">Mission</h1>
                                 <div className="flex-1 h-px bg-gray-400"></div>
                             </div>
-                            <p className="text-justify mb-5">
+                            <p className="text-justify mb-8">
                                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae ipsum adipisci velit voluptate vitae, ullam maiores odio facere maxime autem quis eos atque assumenda itaque voluptates blanditiis dicta soluta culpa! Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, doloremque illum veritatis ad praesentium nulla, recusandae quo odio ab in error veniam temporibus nisi minus dolorem corrupti, commodi inventore iste.
                             </p>
                             <div className="flex items-center my-5">
                                 <h1 className="font-semibold text-2xl mr-4">Vision</h1>
                                 <div className="flex-1 h-px bg-gray-400"></div>
                             </div>
-                            <p className="text-justify">
+                            <p className="text-justify mb-8">
                                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae ipsum adipisci velit voluptate vitae, ullam maiores odio facere maxime autem quis eos atque assumenda itaque voluptates blanditiis dicta soluta culpa! Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, doloremque illum veritatis ad praesentium nulla, recusandae quo odio ab in error veniam temporibus nisi minus dolorem corrupti, commodi inventore iste.
                             </p>
                         </div>
@@ -36,30 +36,61 @@ export default function Landing() {
                             <h2 className="font-semibold text-2xl text-center mr-4">Our Programs</h2>
                             <div className="flex-1 h-px bg-gray-400"></div>
                         </div>
-                        <div className="grid grid-cols-1 sm:grid-cols-1 ">
-                            <div className="flex flex-row items-center">
-                                <img src={img1} alt="FITS Program" className="w-72 h-72 object-cover rounded mb-2" />
-                                <span className="font-medium mx-10">FITS Program Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibus molestiae officiis labore officia voluptatibus ex numquam. Dolor molestiae temporibus iusto quae dolorem non similique impedit debitis asperiores error! Architecto, nihil.</span>
+                        <div className="grid grid-cols-1 sm:grid-cols-1 gap-10">
+                            {/* FITS Program */}
+                            <div className="flex flex-col sm:flex-row items-center sm:items-start mb-10">
+                                <img
+                                    src={img1}
+                                    alt="FITS Program"
+                                    className="w-72 h-72 object-cover rounded mb-2 sm:mb-0 sm:mr-10 mx-auto sm:mx-0"
+                                />
+                                <span className="font-medium mx-0 sm:mx-0 mt-4 sm:mt-0 text-center sm:text-left text-justify">
+                                    FITS Program Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibus molestiae officiis labore officia voluptatibus ex numquam. Dolor molestiae temporibus iusto quae dolorem non similique impedit debitis asperiores error! Architecto, nihil.
+                                </span>
                             </div>
                             <div className="w-full h-px bg-gray-300 my-6"></div>
-                            <div className="flex flex-row items-center">
-                                <span className="font-medium mx-10">Crop Production Lorem ipsum dolor, sit amet consectetur adipisicing elit. Non consequuntur voluptatum provident eius minus ipsam qui possimus rem tempora maxime hic laborum reprehenderit quibusdam labore, recusandae beatae excepturi rerum dolor.</span>
-                                <img src={img2} alt="Crop Production" className="w-72 h-72 object-cover rounded mb-2" />
+                            {/* Crop Production */}
+                            <div className="flex flex-col sm:flex-row items-center sm:items-start mb-10">
+                                <img
+                                    src={img2}
+                                    alt="Crop Production"
+                                    className="w-72 h-72 object-cover rounded mb-2 sm:mb-0 sm:mr-10 mx-auto sm:mx-0"
+                                />
+                                <span className="font-medium mx-0 sm:mx-0 mt-4 sm:mt-0 text-center sm:text-left text-justify">
+                                    Crop Production Lorem ipsum dolor, sit amet consectetur adipisicing elit. Non consequuntur voluptatum provident eius minus ipsam qui possimus rem tempora maxime hic laborum reprehenderit quibusdam labore, recusandae beatae excepturi rerum dolor.
+                                </span>
                             </div>
                             <div className="w-full h-px bg-gray-300 my-6"></div>
-                            <div className="flex flex-row items-center">
-                                <img src={img3} alt="Fisheries Program" className="w-72 h-72 object-cover rounded mb-2" />
-                                <span className="font-medium mx-10">Fisheries Program Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus modi aperiam impedit unde quod a voluptatem rerum aliquam possimus incidunt. Praesentium, excepturi blanditiis doloribus dolore dolor repellat doloremque quis officiis?</span>
+                            {/* Fisheries Program */}
+                            <div className="flex flex-col sm:flex-row items-center sm:items-start mb-10">
+                                <img
+                                    src={img3}
+                                    alt="Fisheries Program"
+                                    className="w-72 h-72 object-cover rounded mb-2 sm:mb-0 sm:mr-10 mx-auto sm:mx-0"
+                                />
+                                <span className="font-medium mx-0 sm:mx-0 mt-4 sm:mt-0 text-center sm:text-left text-justify">
+                                    Fisheries Program Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus modi aperiam impedit unde quod a voluptatem rerum aliquam possimus incidunt. Praesentium, excepturi blanditiis doloribus dolore dolor repellat doloremque quis officiis?
+                                </span>
                             </div>
                             <div className="w-full h-px bg-gray-300 my-6"></div>
-                            <div className="flex flex-row items-center">
-                                <span className="font-medium mx-10">Organic Farming Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptate quod doloribus at exercitationem voluptates eaque possimus non necessitatibus ut laborum soluta vitae quia, incidunt ipsam labore suscipit recusandae fugiat quasi.</span>
-                                <img src={img4} alt="Organic Farming" className="w-72 h-72 object-cover rounded mb-2" />      
+                            {/* Organic Farming */}
+                            <div className="flex flex-col sm:flex-row items-center sm:items-start mb-10">
+                                <img
+                                    src={img4}
+                                    alt="Organic Farming"
+                                    className="w-72 h-72 object-cover rounded mb-2 sm:mb-0 sm:mr-10 mx-auto sm:mx-0"
+                                />
+                                <span className="font-medium mx-0 sm:mx-0 mt-4 sm:mt-0 text-center sm:text-left text-justify">
+                                    Organic Farming Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptate quod doloribus at exercitationem voluptates eaque possimus non necessitatibus ut laborum soluta vitae quia, incidunt ipsam labore suscipit recusandae fugiat quasi.
+                                </span>
                             </div>
                             <div className="w-full h-px bg-gray-300 my-6"></div>
-                            <div className="flex flex-col items-center">
-                                <img src={img5} alt="Rabies Control" className="w-72 h-72 object-cover rounded mb-2" />
-                                <span className="font-medium my-10">Rabies Control Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime quam deserunt voluptatem assumenda possimus repellendus exercitationem expedita, molestiae itaque perspiciatis dolorum nisi dignissimos ex non sapiente veniam velit! At, dolore.</span>
+                            {/* Rabies Control */}
+                            <div className="flex flex-col items-center mb-10">
+                                <img src={img5} alt="Rabies Control" className="w-72 h-72 object-cover rounded mb-2 mx-auto" />
+                                <span className="font-medium my-10 text-center text-justify">
+                                    Rabies Control Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime quam deserunt voluptatem assumenda possimus repellendus exercitationem expedita, molestiae itaque perspiciatis dolorum nisi dignissimos ex non sapiente veniam velit! At, dolore.
+                                </span>
                             </div>
                         </div>
                     </div>
