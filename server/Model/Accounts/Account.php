3e1c1d0f82cd278e1540ebfac78e4337aaeff55a
Kc Sean Brix Calinao <kcseancalinao@gmail.com>
2025-05-31 17:38:36 +0800

    v0.16.1 - Tailwind CLI inserted to the client side

--- server/Model/Accounts/Account.php
+++ server/Model/Accounts/Account.php
@@ -4,6 +4,7 @@ require_once __DIR__."/../../Utils/statement.php";
 class Account {
 
     private $id = null;
+    private $access = "User";
     private $firstname = null;
     private $lastname = null;
     private $gender = null;
@@ -27,18 +28,36 @@ class Account {
         if($account_id) $this->initialized($account_id);
     }
 
-    public function print($key){
-        if(property_exists($this, $key)) {
-            echo $this->$key ?? '';
-        }
+    public function getDetails(){
+        return [
+            'id' => $this->id,
+            'access' => $this->access,
+            'firstname' => $this->firstname,
+            'lastname' => $this->lastname,
+            'gender' => $this->gender,
+            'client_profile' => $this->client_profile,
+            'address' => $this->address,
+            'telephone_no' => $this->telephone_no,
+            'cellphone_no' => $this->cellphone_no,
+            'occupation' => $this->occupation,
+            'position' => $this->position,
+            'institution' => $this->institution,
+            'email_address' => $this->email_address,
+            'username' => $this->username,
+            'password' => $this->password,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at
+        ];
     }
 
     public function initialized($account_id){
+        session_start();
 
         $query = "SELECT * FROM `accounts` WHERE id= ?";
         $result = statement($query, [$account_id], "s");
 
         while($row = mysqli_fetch_assoc($result)){
+            $this->access = $row["access"];
             $this->firstname = $row["firstname"];
             $this->lastname = $row["lastname"];
             $this->gender = $row["gender"];
@@ -55,5 +74,8 @@ class Account {
             $this->created_at = $row["created_at"];
             $this->updated_at = $row["updated_at"];
         }
+
+        $_SESSION["ID"] = $this->id;
+        $_SESSION["username"] = $this->username;
     }
 }
