a0494b7438fb34b35ab66eadf15af507e0a7e5b0
Kc Sean Brix Calinao <kcseancalinao@gmail.com>
2025-06-02 17:28:02 +0800

    v0.26.2 - fixed logs autdit not included in git

---
+++ client/src/Admin/Services/Logs/Audit.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+
+function Audit() {
+
+  return (
+    <div className='flex flex-col justify-center items-center font-bold bg-pink-300 w-full h-[91%] mt-[5%]'>
+        <h1>Logs / Audit Trails</h1>
+    </div>
+  )
+}
+
+export default Audit;
