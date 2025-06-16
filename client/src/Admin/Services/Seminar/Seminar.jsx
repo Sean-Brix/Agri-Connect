01da54e14a813a1931f251e84551cda2992ff072
Kc Sean Brix Calinao <kcseancalinao@gmail.com>
2025-06-16 18:51:21 +0800

    v0.61.1 - Minor detailing to better represent seminar status for ongoing and completed

--- client/src/Admin/Services/Seminar/Seminar.jsx
+++ client/src/Admin/Services/Seminar/Seminar.jsx
@@ -341,9 +341,9 @@ export default function Seminar({ admin_navigate }) {
                                 />
                                 <span className={`absolute top-4 right-4 px-4 py-1 rounded-full text-xs font-bold shadow ${
                                     item.status === 'Ongoing'
-                                        ? 'bg-green-100 text-green-700'
+                                        ? 'bg-yellow-100 text-yellow-700'
                                         : item.status === 'Completed'
-                                        ? 'bg-gray-200 text-gray-600'
+                                        ? 'bg-green-200 text-green-600'
                                         : item.status === 'Cancelled'
                                         ? 'bg-red-100 text-red-600'
                                         : 'bg-blue-100 text-blue-700'
